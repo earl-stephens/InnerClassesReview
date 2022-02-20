f980b3293ad705c53a02bb0c0eaf886ab1981995
earl-stephens <stephensearl@hotmail.com>
2022-02-19 23:58:25 -0700

    Use four types of inner classes

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,6 @@
 package application;
 
-public class App {
+public class App implements Runnable {
 	
 	private String name = "Mickey Mouse";
 
@@ -10,6 +10,7 @@ public class App {
 	}
 	
 	public void start() {
+		//anonymous class
 		Runnable runner1 = new Runnable() {
 			public void run() {
 				System.out.println("1 " + name);
@@ -17,13 +18,27 @@ public class App {
 		};
 		activate(runner1);
 		
+		//non static inner class
 		Runner2 runner2 = new Runner2();
 		activate(runner2);
 		
+		//static inner class
 		Person person = new Person(name);
 		Person.Display showName = person.new Display();
 		activate(showName);
+		
+		//implement Runnable in main class
+		activate(this);
+		
+		//method local inner class
+		class Runner3 implements Runnable {
+			public void run() {
+				System.out.println("5 " + name);
+			}
+		}
+		activate(new Runner3());
 	}
+
 	
 	public void activate(Runnable runnable) {
 		runnable.run();
@@ -33,6 +48,10 @@ public class App {
 		public void run() {
 			System.out.println("2 " + name);
 		}
+	}
+	
+	public void run() {
+		System.out.println("4 " + name);
 	}
 
 }
