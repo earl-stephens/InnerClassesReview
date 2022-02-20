52c4c279621f3ce95efce93882b741255527524b
earl-stephens <stephensearl@hotmail.com>
2022-02-19 23:49:16 -0700

    Work on exercise solution

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,38 @@
 package application;
 
 public class App {
+	
+	private String name = "Mickey Mouse";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new App().start();
 
+	}
+	
+	public void start() {
+		Runnable runner1 = new Runnable() {
+			public void run() {
+				System.out.println("1 " + name);
+			}
+		};
+		activate(runner1);
+		
+		Runner2 runner2 = new Runner2();
+		activate(runner2);
+		
+		Person person = new Person(name);
+		Person.Display showName = person.new Display();
+		activate(showName);
+	}
+	
+	public void activate(Runnable runnable) {
+		runnable.run();
+	}
+	
+	public class Runner2 implements Runnable {
+		public void run() {
+			System.out.println("2 " + name);
+		}
 	}
 
 }
